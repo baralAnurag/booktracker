89316a8d486817d297603f440e885b88fac2453c
Anurag Baral <anuragbaral@Anurags-Mac.local>
2019-04-02 10:43:12 -0400

    Error Handling, resolvers

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from 'app/models/reader';
 import { Book } from 'app/models/book';
 import { BookTrackerError } from 'app/models/bookTrackerError';
 import { OldBook } from 'app/models/oldBook';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, catchError} from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
@@ -28,11 +28,21 @@ export class DataService {
     return allReaders.find(reader => reader.readerID === id);
   }
 
-  getAllBooks(): Observable<Book[]> {
+  getAllBooks(): Observable<Book[] | BookTrackerError> {
     console.log('Getting all books from the server');
-    return this.http.get<Book[]>('/api/books');
+    return this.http.get<Book[]>('/api/books')
+    .pipe(
+      catchError(err => this.handleHttpError(err))
+    );
   }
 
+  private handleHttpError(error: HttpErrorResponse): Observable<BookTrackerError> {
+   const dataError = new BookTrackerError();
+   dataError.errorNumber = 100;
+   dataError.message = error.statusText;
+   dataError.friendlyMessage = 'An Error occurred retrirving data.';
+   return throwError(dataError);
+  }
   getBookById(id: number): Observable<Book> {
     return this.http.get<Book>(`/api/books/${id}`, {
       headers: new HttpHeaders({
